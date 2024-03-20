b36730dcd9fec5da16a7a541059be0494132e0ad
ahmed moussa <ahmed.mousa.20151@gmail.com>
2024-03-20 15:31:21 +0100

    fix interceptor

--- src/app/global-component.ts
+++ src/app/global-component.ts
@@ -1,12 +1,12 @@
 export const GlobalComponent = {
     // Api Calling
-    API_URL: 'https://api-node.themesbrand.website/',
-    // API_URL : 'http://127.0.0.1:3000/',
+    //API_URL: 'https://api-node.themesbrand.website/',
+    API_URL : 'http://localhost:8080/api/',
     headerToken: { 'Authorization': `Bearer ${localStorage.getItem('token')}` },
 
     // Auth Api
-    AUTH_API: "https://api-node.themesbrand.website/auth/",
-    // AUTH_API:"http://127.0.0.1:3000/auth/",
+    //AUTH_API: "https://api-node.themesbrand.website/auth/",
+    AUTH_API:"http://localhost:8080/api/auth/",
 
 
     // Products Api
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,21 +3,20 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  baseUrl: 'http://localhost:8091/api/v1',
-
-  defaultauth: 'fakebackend',
+  production: true,
+  defaultauth: 'fackbackend',
   firebaseConfig: {
-    apiKey: 'AIzaSyCqS9cSPrDCNSQ-Ku2kZf5DBWjPPv7hvcA',
-    authDomain: 'test-demo-774f8.firebaseapp.com',
-    databaseURL: 'https://test-demo-774f8-default-rtdb.firebaseio.com',
-    projectId: 'test-demo-774f8',
-    storageBucket: 'test-demo-774f8.appspot.com',
-    messagingSenderId: '916438010670',
-    appId: '1:916438010670:web:c70cf404da6c0fe7b048bf',
-    measurementId: 'G-1N6FB2GG55',
-  },
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: ''
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
